f2525ca3778ba1bb28846b12b43d071774bd3e0b
Michal Zielinski <zielinski93.m@gmail.com>
2023-11-28 21:29:14 +0100

    30.3 Assignment

--- kodilla-jdbc/src/main/java/com/kodilla/jdbc/DbManager.java
+++ kodilla-jdbc/src/main/java/com/kodilla/jdbc/DbManager.java
@@ -5,13 +5,12 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
-public enum DbManager {
-
-    INSTANCE;
+public class DbManager {
 
     private Connection conn;
+    private static DbManager dbManagerInstance;
 
-    DbManager() {
+    private DbManager() throws SQLException {
         Properties connectionProps = new Properties();
         connectionProps.put("user", "kodilla_user");
         connectionProps.put("password", "kodilla_Pass123");
@@ -26,8 +25,11 @@ public enum DbManager {
         }
     }
 
-    public static DbManager getInstance() {
-        return INSTANCE;
+    public static DbManager getInstance() throws SQLException {
+        if (dbManagerInstance == null) {
+            dbManagerInstance = new DbManager();
+        }
+        return dbManagerInstance;
     }
 
     public Connection getConnection() {
--- kodilla-patterns2/src/main/java/com/kodilla/patterns2/KodillaPatterns2Application.java
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/KodillaPatterns2Application.java
@@ -1,4 +1,4 @@
-package com.kodilla.patterns2.facade;
+package com.kodilla.patterns2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
